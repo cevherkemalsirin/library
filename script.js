4d363d0ead7cf87a125d1b16912628ed8a5ff000
cevher kemal sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2024-04-10 16:13:06 +0300

    book

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 "use strict"
 
+
+function Book(title, author, pageNum, isRead)
+{
+    title,
+    author,
+    pageNum,
+    isRead
+}
+
+Book.prototype.Info()
+{
+    let isReadStr = isRead ? "is read." : "is not read yet."
+    return `${title} by ${author} ${pageNum} pages ${isReadStr}`;
+}
+
