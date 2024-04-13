6eac7f90534b2bdaf7f8ed4784c53b4d5bec48f7
cevher kemal sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2024-04-13 22:21:45 +0300

    add JS code to add a book

--- script.js
+++ script.js
@@ -3,22 +3,70 @@
 const bookModal = document.querySelector(".bookModal");
 const modalCloseBtn = document.querySelector(".closeBtn");
 const bookAddBtn = document.querySelector(".addBookBtn");
+const submitBtn = document.querySelector(".bookSubmit");
+const cardContainer = document.querySelector(".bottom-part");
+
+//Form variables for book
+const iBookName = document.querySelector("#bookName");
+const iPageNumber = document.querySelector("#pageNum");
+const iBookAuthor = document.querySelector("#author");
+const iBookIsread = document.querySelector("#isRead");
 
 const libraryBooks = [];
 
 function Book(title, author, pageNum, isRead)
 {
-    title,
-    author,
-    pageNum,
-    isRead
+    this.title = title,
+    this.author = author,
+    this.pageNum = pageNum,
+    this.isRead = isRead
 };
 
 Book.prototype.Info = function()
 {
-    let isReadStr = isRead ? "is read." : "is not read yet.";
-    return `${title} by ${author} ${pageNum} pages ${isReadStr}`;
+    let isReadStr= this.isRead ?  "is read." : "is not read yet.";
+    return `${this.title} by ${this.author} ${this.pageNum} pages ${isReadStr}`;
 };
+
+Book.prototype.ToggleRead = function()
+{
+    if(this.isRead !== undefined)
+    {
+      this.isRead = !this.isRead;
+    }
+}
+
+function AddBookToLibrary(){
+    const newBook = new Book(
+        iBookName.value,
+        iBookAuthor.value,
+        iPageNumber.value,
+        iBookIsread.checked
+    );
+    console.log(newBook.Info());
+    libraryBooks.push(newBook);
+    UpdateHtml(newBook);
+}
+
+function UpdateHtml(newbook){
+cardContainer.insertAdjacentHTML("beforeend", `  
+                <div class="card">
+                <p class="bookTitle">
+                    ${newbook.title}
+                </p>
+                <p class="pageNum">
+                    ${newbook.pageNum}
+                </p>
+                <p class="author">
+                     ${newbook.author}
+                </p>
+                <form action="">
+                    <label for="isRead1">Read</label>
+                    <input type="checkbox" id="isRead1" name="read">
+                </form>
+                <img src="" alt="remove" class="remove">
+            </div>`);
+}
 
 bookAddBtn.addEventListener("click", ()=>{
     bookModal.showModal();
@@ -26,4 +74,16 @@ bookAddBtn.addEventListener("click", ()=>{
 
 modalCloseBtn.addEventListener("click", ()=>{
     bookModal.close();
+});
+
+
+submitBtn.addEventListener("click", (e)=>{
+
+    if (iBookName.value && iPageNumber.value && iBookAuthor.value) 
+    {
+       AddBookToLibrary();
+       e.preventDefault();
+       bookModal.close();
+    }
+
 });
